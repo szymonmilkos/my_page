9a4db1b08e09ce046b8cb41ec4722b6eb8f55f57
szymonmilkos <101351024+szymonmilkos@users.noreply.github.com>
2025-05-17 11:11:26 +0200

    Update index.ts 23

--- server/index.ts
+++ server/index.ts
@@ -57,7 +57,7 @@ app.use((req, res, next) => {
   }
 
 
-  const port = process.env.PORT || 8080;
+  const port = process.env.PORT ? parseInt(process.env.PORT) : 5000;
     app.listen(port, () => {
     console.log(`Listening on port ${port}`);
   });
